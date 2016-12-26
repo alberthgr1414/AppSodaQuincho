35724f2a87bf1f74b7a412c7303aa1fd92ec737f
Alberth <Alberth@DESKTOP-8K8PJ62>
2016-12-26 09:12:31 -0600

    Modificacion para abrir turnos

--- DAL/TurnoDAL.cs
+++ DAL/TurnoDAL.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades;
 
 namespace DAL
 {
     public class TurnoDAL
     {
+        public static DataTable UltimoTurnoAbierto(int tipoPlato)
+        {
+            //Declaración de objeto SqlCommand           
+            SqlCommand oCommand = new SqlCommand();
+            //Declaración de Objeto DataTable
+            DataTable oDataTable = new DataTable();
+            oCommand.CommandText = "SpTurnoListar";
+            oCommand.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                return PersistenciaSqlServer.Persistencia.Persistencia.getInstance().EjecutarConsultaDataTable(oCommand);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                oDataTable.Dispose();
+            }
+        }
+
+        public static int GuardarPlato(Turno pTurno)
+        {
+            //Declaración de objeto SqlCommand
+            SqlCommand oCommand = new SqlCommand();
+            int registrosActualizados = 0;
+            oCommand.CommandText = "SpTurnoInsertar";
+            oCommand.CommandType = CommandType.StoredProcedure;
+
+            //Crear los Parámetros del procedimiento y sus valores
+            oCommand.Parameters.AddWithValue("@ID_Usuario", 1);
+            oCommand.Parameters[0].Direction = ParameterDirection.Input;
+
+            oCommand.Parameters.AddWithValue("@Monto", 1);
+            oCommand.Parameters[1].Direction = ParameterDirection.Input;
+
+            try
+            {
+                registrosActualizados = PersistenciaSqlServer.Persistencia.Persistencia.getInstance().EjecutarSqlActualizacion(oCommand);
+                return registrosActualizados;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
--- Entidades/Turno.cs
+++ Entidades/Turno.cs
@@ -7,5 +7,10 @@ namespace Entidades
 {
     public class Turno
     {
+        public int ID_Turno { set; get; }
+        public int ID_Usuario { set; get; }
+        public DateTime Apertura { set; get; }
+        public DateTime Cierre { set; get; }
+        public int Estado { set; get; }
     }
 }
